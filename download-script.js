fa10b2468d508bacd97f7d930bde68bb0b33c409
RandilT <randiltharusha72@gmail.com>
2024-02-13 14:04:35 +0530

    script to download all images of brawlers

---
+++ download-script.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const axios = require("axios");
+const brawlers = require("./brawl-stars/brawl_stars_brawlers.json");
+async function downloadImages(baseUrl, outputFolder) {
+  // Create the output folder if it doesn't exist
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder, { recursive: true });
+  }
+
+  // Iterate through the image names
+  for (const brawler of brawlers.items) {
+    try {
+      // Construct the image URL
+      const imageUrl = `${baseUrl}/${brawler.name
+        .charAt(0)
+        .toUpperCase()}${brawler.name.slice(1).toLowerCase()}.png`;
+
+      // Send a GET request to download the image
+      const response = await axios.get(imageUrl, {
+        responseType: "arraybuffer",
+      });
+
+      // Save the image to the output folder
+      fs.writeFileSync(
+        `${outputFolder}/${brawler.name.charAt(0).toUpperCase()}${brawler.name
+          .slice(1)
+          .toLowerCase()}.png`,
+        response.data
+      );
+      console.log(
+        `Downloaded ${brawler.name.charAt(0).toUpperCase()}${brawler.name
+          .slice(1)
+          .toLowerCase()}.png`
+      );
+    } catch (error) {
+      console.error(
+        `Failed to download ${brawler.name
+          .charAt(0)
+          .toUpperCase()}${brawler.name.slice(1).toLowerCase()}.png: ${
+          error.message
+        }`
+      );
+    }
+  }
+}
+
+const baseUrl = ""; // Base URL
+const outputFolder = "./brawl-stars/brawler-images"; // Output folder to save the images
+
+downloadImages(baseUrl, outputFolder);
